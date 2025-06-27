1f44e83b05978c80a2a5c50348e31d62cef6cc4a
Synapse-LeeGW <lgn@synapseimaging.co.kr>
2025-06-28 03:47:57 +0900

    Message Text

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  //아래 코드 추가
+
+module.exports = {
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://github.com/ObedLee/Antioch/"
       : "",
 };
- 
-module.exports = nextConfig;
-
-
-module.exports = nextConfig;
